204a41bb09abfa4f6be079662a1100607e425d45
Dilitov <tfutka@gmail.com>
2022-08-25 18:47:01 +0300

    Исправленный

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,17 +8,29 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 public class Main {
-       public static void main(String[] args) {
-//        Util.getConnection();
-        UserDao userDao = new UserDaoJDBCImpl();
-//        userDao.createUsersTable();
-//        userDao.saveUser("Name1", "LastName1", (byte) 20);
-//        userDao.saveUser("Name2", "LastName2", (byte) 25);
-//        userDao.saveUser("Name3", "LastName3", (byte) 31);
-//        userDao.saveUser("Name4", "LastName4", (byte) 38);
-        userDao.removeUserById(2);
-//        userDao.getAllUsers();
-//        userDao.cleanUsersTable();
-//        userDao.dropUsersTable();
+    private static final UserService userService = new UserServiceImpl();
+    private static final User user1 = new User("Name1", "LastName1", (byte) 20);
+    private static final User user2 = new User("Name2", "LastName2", (byte) 25);
+    private static final User user3 = new User("Name3", "LastName3", (byte) 31);
+    private static final User user4 = new User("Name4", "LastName4", (byte) 38);
+
+    public static void main(String[] args) {
+        userService.createUsersTable();
+
+        userService.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+
+        userService.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+
+        userService.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+
+        userService.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
+        Util.closeConnection();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,23 +7,36 @@ import java.sql.SQLException;
 
 
 public class Util {
+    private static  Connection connection = null;
     private static final String URl = "jdbc:mysql://localhost:3306/sakila";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
     public static Connection getConnection() {
-        Connection connection = null;
         try {
             connection = DriverManager.getConnection(URl, USERNAME, PASSWORD);
             if (!connection.isClosed()) {
                 System.out.println("Connection OK");
             }
-//            connection.close();
+
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Connection ERROR");
         }
         return connection;
     }
+    public static void closeConnection() {
+        try {
+            if (!connection.isClosed() || connection == null) {
+                connection.close();
+                System.out.println("Connection CLOSE");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection was NOT created");
+        }
+
+    }
+
 }
 
